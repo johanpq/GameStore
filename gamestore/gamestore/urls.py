7dfe05dc15834da388200c55d0b4443f8085e57e
letsticia <leti.morais48@gmail.com>
2024-05-16 17:20:00 -0300

    feat: added Games and Category urls

--- gamestore/gamestore/urls.py
+++ gamestore/gamestore/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, path
+from games.views import GameViewSet, CategoryViewSet
+
+game_view = GameViewSet.as_view({"get": "list", "post": "create"})
+category_view = CategoryViewSet.as_view({"get": "list", "post": "create"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('games/', game_view),
+    path('category/', category_view),
+] 
