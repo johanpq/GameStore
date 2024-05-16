6761befb25fd7dfb835dcecd8b7faf7c0d1dffd7
letsticia <leti.morais48@gmail.com>
2024-05-16 17:19:14 -0300

    feat: added Games and Category models

--- gamestore/games/models.py
+++ gamestore/games/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Games(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='images/')
+    description = models.TextField()
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
