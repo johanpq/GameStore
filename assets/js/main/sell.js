26cdbcbcd20e465bef442d211a1745e3b2d5f2d6
johanpq <johanp.queiroz@gmail.com>
2024-06-11 14:45:01 -0300

    feat: added event to pass the ID through URL

--- assets/js/main/sell.js
+++ assets/js/main/sell.js
@@ -17,7 +17,7 @@ async function showGameDetails() {
     if (id) {
         try {
             const game = await fetchGameById(id);
-            displayGameDetails(game);
+            displayGameDetails(game, id);
         } catch (error) {
             console.error('Failed to fetch game:', error);
         }
@@ -26,7 +26,7 @@ async function showGameDetails() {
     }
 }
 
-function displayGameDetails(game) {
+function displayGameDetails(game, id) {
     const imgGame = document.createElement('img');
     imgGame.setAttribute('src', game.image);
     imgGame.setAttribute('alt', game.title);
@@ -38,51 +38,11 @@ function displayGameDetails(game) {
     document.getElementById('price-game').innerHTML = " R$ " + game.price;
 
     document.getElementById('description').innerHTML = game.description;
-}
+
+    document.getElementById('cart-link').addEventListener('click', () => {
+        const cartPageUrl = `../pages/cart.html?id=${id}`;
+        window.location.href = cartPageUrl; // Redirecting to the cart page
+    });
+} 
 
 document.addEventListener('DOMContentLoaded', showGameDetails);
-
-
-/*
-
-const createFreeGame = (games) => {
-    const h3Title = "Play games for free";
-
-    const testemunhal = document.createElement('div');
-    testemunhal.classList.add('testemunhal');
-
-    const h3 = document.createElement('h3');
-    h3.textContent = h3Title;
-
-    const testemunhalContainer = document.createElement('div');
-    testemunhalContainer.classList.add('testemunhal-container');
-
-    testemunhal.appendChild(h3);
-    testemunhal.appendChild(testemunhalContainer);
-    mainContainer.appendChild(testemunhal);
-
-    const gameTitle0 = "Elden Ring";
-    const gameImg0 = "assets/images/games/elden-ring-2k-wallpaper.jpg";
-    const gameImgAlt0 = "Elden Ring";
-    const littleDescrip0 = "Epic RPG adventure";
-    const gamePrice0 = "R$ 00";
-
-    const gameTitle1 = "League of Legends";
-    const gameImg1 = "assets/images/games/lol-evelyn.jpg";
-    const gameImgAlt1 = "League of Legends";
-    const littleDescrip1 = "Intense MOBA battles";
-    const gamePrice1 = "R$ 00";
-
-    const gameTitle2 = "Counter Strike 2";
-    const gameImg2 = "assets/images/games/counter-strike-2-game-2k-wallpaper-uhdpaper.com-222@1@k.jpg";
-    const gameImgAlt2 = "Counter Strike 2";
-    const littleDescrip2 = "Tactical online shooter";
-    const gamePrice2 = "R$ 00";
-
-    CreateGames(testemunhalContainer, gameTitle0, gameImg0, gameImgAlt0, littleDescrip0, gamePrice0);
-    CreateGames(testemunhalContainer, gameTitle1, gameImg1, gameImgAlt1, littleDescrip1, gamePrice1);
-    CreateGames(testemunhalContainer, gameTitle2, gameImg2, gameImgAlt2, littleDescrip2, gamePrice2)   
-}; 
-
-
-*/
